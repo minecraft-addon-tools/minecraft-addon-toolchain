1e3e6321ff480421208015581a8f60e43bbe8631
Steven Blom <atomicblom@binaryvibrance.net>
2019-01-19 23:46:17 +1030

    Tests should use documented method of importing V2

--- tests/v2-typescript/gulpfile.js
+++ tests/v2-typescript/gulpfile.js
@@ -1,9 +1,9 @@
-const MinecraftAddonBuilder = require("minecraft-addon-toolchain");
+const MinecraftAddonBuilder = require("minecraft-addon-toolchain/v2");
 const TypeScriptSupport = require("minecraft-addon-toolchain-typescript");
 const BrowserifySupport = require("minecraft-addon-toolchain-browserify");
 const TerserSupport = require("minecraft-addon-toolchain-terser");
 
-const builder = new MinecraftAddonBuilder.v2("ToolchainTest");
+const builder = new MinecraftAddonBuilder("ToolchainTest");
 
 builder.addPlugin(new TypeScriptSupport(builder));
 builder.addPlugin(new TerserSupport(builder));
