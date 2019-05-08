ccfdb11554ca7e2a5a5e327d43772b655e37d0ba
jaxx2104 <jaxx2104@gmail.com>
2019-05-08 23:13:40 +0900

    fix tab width

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true
-  },
-  extends: ["plugin:prettier/recommended"],
-  parserOptions: {
-    ecmaVersion: 2017
-  },
-  rules: {
-    "prettier/prettier": "error"
-  }
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true
+    },
+    extends: ["plugin:prettier/recommended"],
+    parserOptions: {
+        ecmaVersion: 2017
+    },
+    rules: {
+        "prettier/prettier": [
+            "error",
+            {
+                tabWidth: 4
+            }
+        ]
+    }
 };
